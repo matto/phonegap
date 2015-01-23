5c8a7dc493d06979ef375a9f6e6674bb9c1853ea
matto1 <matiasditata@gmail.com>
2015-01-23 03:00:37 +0000

    ultimo intento

--- src/states/Facebook.js
+++ src/states/Facebook.js
@@ -7,17 +7,22 @@ VendorAttack.Facebook.prototype = {
     },
     
     create: function() {
-        this.facebookConnectPlugin.login(["email"], loginSuccess, loginFailure);
-        window.facebookConnectPlugin.login(["email"], loginSuccess, loginFailure);
+        if(this.facebookConnectPlugin) console.log("this.facebookConnectPlugin");
+        if(this.facebookConnectPlugin.login) console.log("this.facebookConnectPlugin.login");
+        this.facebookConnectPlugin.login(["email"], this.loginSuccess, this.loginFailure);
+        if(window.facebookConnectPlugin) console.log("window.facebookConnectPlugin");
+        if(window.facebookConnectPlugin.login) console.log("window.facebookConnectPlugin.login");
+       window.facebookConnectPlugin.login(["email"], this.loginSuccess, this.loginFailure);
+        console.log(this.facebookConnectPlugin, window.facebookConnectPlugin, this.facebookConnectPlugin === window.facebookConnectPlugin);
 
         //this.state.start('Preloader');
     },
     
-    loginSuccess: function() {
-        console.log("loginSuccess");
+    loginSuccess: function(userData) {
+        console.log("loginSuccess: ", userData);
     },
     
-    loginFailure: function() {
-        console.log("loginFailure");
+    loginFailure: function(error) {
+        console.log("loginFailure: ", error);
     }
 };
